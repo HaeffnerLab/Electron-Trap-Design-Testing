fixed serious BUG about inferring E field of the other three quadrants given the data from the first quadrants. E_x(x,y)=-E_x(-x, y). I missed the negative sign previously...

particle_trajectory_sim/trap_util.py
@@ -418,6 +418,7 @@ class quarter_trap(trap):
 
 	def E_field(self, x, y, t):
 	    """for documentation, see the method with the same name under class trap"""
+	    x_real, y_real = x, y
 	    x, y = abs(x), abs(y)
 	    if x > self.x_max:
 	        x = self.x_max
@@ -427,8 +428,8 @@ class quarter_trap(trap):
 	        y = self.y_max
 	    if y < self.y_min:
 	        y = self.y_min
-	    Ex = self.intp_Ex(x, y)[0][0]
-	    Ey = self.intp_Ey(x, y)[0][0]
+	    Ex = self.intp_Ex(x, y)[0][0] * (x_real/x)
+	    Ey = self.intp_Ey(x, y)[0][0] * (y_real/y)
 	    return (Ex*np.cos(2*np.pi*self.f*t), Ey*np.cos(2*np.pi*self.f*t))
 
 	def within_boundary(self, x, y):
@@ -481,6 +482,7 @@ class half_trap(trap):
 	
 	def E_field(self, x, y, t):
 	    """for documentation, see the method with the same name under class trap"""
+	    x_real = x
 	    x = abs(x)
 	    if x > self.x_max:
 	        x = self.x_max
@@ -490,7 +492,7 @@ class half_trap(trap):
 	        y = self.y_max
 	    if y < self.y_min:
 	        y = self.y_min
-	    Ex = self.intp_Ex(x, y)[0][0]
+	    Ex = self.intp_Ex(x, y)[0][0] * x_real/x
 	    Ey = self.intp_Ey(x, y)[0][0]
 	    return (Ex*np.cos(2*np.pi*self.f*t), Ey*np.cos(2*np.pi*self.f*t))
 